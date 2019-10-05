ccf92fd9b16513d06078f04b2d3655162103b96d
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-10-05 14:54:26 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SomeClassLib1.Class1 c = new SomeClassLib1.Class1();
+                c.DoSomething();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ SomeClassLib1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeClassLib1
+{
+    public class Class1
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Inside method DoSomething");
+        }
+    }
+}
