8fa14d9b19913f77f04f702aea3e40408c3fe9ef
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-10-06 20:22:56 +0100

    Added test with deployment dir

--- UnitTestProject1Fwk/UnitTest1.cs
+++ UnitTestProject1Fwk/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1Fwk
@@ -12,5 +13,15 @@ namespace UnitTestProject1Fwk
             var o = new SomeClassLib1.Class1();
             o.DoSomething();
         }
+        [TestMethod]
+        [DeploymentItem("Junk.txt")]
+        public void MyTestMethodWithDeployment()
+        {
+            var assem = System.Reflection.Assembly.GetExecutingAssembly();
+            string locn = assem.Location;
+            Trace.WriteLine($"1 Inside method {nameof(MyTestMethodWithDeployment)} Location of assembly={locn}");
+            Trace.WriteLine($"Deployment direc={TestContext.DeploymentDirectory}");
+        }
+        public TestContext TestContext { get; set; }
     }
 }
